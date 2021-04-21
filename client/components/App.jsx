50795cb0eb6ccc99f2e51b9f8ccbbcb57afd7c24
Sarah Smith <s.smith.nz@gmail.com>
2021-04-22 07:40:25 +1200

    add hardbode map

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Header />
         <ShowImage/>
         <Body />
-        <MapShow/>
+        <MapShow />
       </div>
     </div>
   )
---
+++ client/components/MapShow.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function MapShow () {
+  return (
+    <>
+
+      <nav className="relative flex border-t-2 py-3 flex-col mx-auto items-center justify-center px-2 py-3 bg-green-900 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-center">
+          <div className="text-white text-center  text-xl w-full relative flex justify-center">
+
+            <img src="../images/MapFill.JPG"width='auto' height='auto'/>
+
+          </div>
+        </div>
+
+      </nav>
+    </>
+  )
+}
+
+export default MapShow
