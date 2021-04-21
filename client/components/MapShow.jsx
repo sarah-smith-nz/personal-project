d9198bbceb4ad04d6c0c720c5a41b88f453c2b8e
Sarah Smith <77254910+sarah-smith-nz@users.noreply.github.com>
2021-04-22 11:53:08 +1200

    add video

--- client/components/MapShow.jsx
+++ client/components/MapShow.jsx
@@ -4,7 +4,7 @@ function MapShow () {
   return (
     <>
 
-      <nav className="relative flex border-t-2 py-3 flex-col mx-auto items-center justify-center px-2 py-3 bg-green-900 mb-3">
+      <nav className="relative flex border-t-2 py-3  px-2 py-3 bg-green-900 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-center">
           <div className="text-white text-center  text-xl w-full relative flex justify-center">
 
@@ -12,13 +12,29 @@ function MapShow () {
 
           </div>
         </div>
-        <div className="mt-5 sm:mt-8 sm:flex sm:justify-right lg:justify-right">
-          <div className="rounded-md shadow">
-            <a href="/west" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+        <div className="mt-2 mb-5 mx-8 flex-col items-start justify-start sm:flex m:flex lg:flex">
+          <div className="mt-5 rounded-md shadow">
+            <a href="/north" className="w-full flex items-center justify-center text-center  px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+                North Shore
+            </a>
+          </div>
+          <div className="mt-5 rounded-md shadow">
+            <a href="/west" className="w-full flex items-center justify-center text-center  px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
                 West Auckland
             </a>
           </div>
+          <div className="mt-5 rounded-md shadow">
+            <a href="/central" className="w-full flex items-center justify-center text-center  px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+                Central Auckland
+            </a>
+          </div>
+          <div className="mt-5 rounded-md shadow">
+            <a href="/south" className="w-full flex items-center justify-center text-center  px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+                South Auckland
+            </a>
+          </div>
         </div>
+
       </nav>
     </>
   )
--- client/components/West.jsx
+++ client/components/West.jsx
@@ -5,15 +5,20 @@ function West () {
     <>
       <nav className="relative flex border-t-2 py-3 flex-col mx-auto items-center justify-center px-2 py-3 bg-green-900 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-center">
-          <div className="text-white text-center  text-xl w-full relative flex justify-center">
+          <div className="text-white text-center flex-col text-xl w-full relative flex justify-center">
             WEST AUCKLAND<br></br>
-            Auckland's Home Ground for Mountain Biking. Our newly built 50km+ network is NZ's most frequented and has something for every level of rider. We're 40 minutes from downtown Auckland, have 75+ hire bikes, an onsite mechanic service, proshop &small cafe.
+            Auckland's Home Ground for Mountain Biking. Our newly built 50km+ network is NZ's most frequented and has something for every level of rider. We're 40 minutes from downtown Auckland, have 75+ hire bikes, an onsite mechanic service, proshop & small cafe.<br></br>
+            <img src="../images/Woodhilltrails.png" width='300px' height='auto'/>
+            <img src="../images/Woodhilltrails.png" width='300px' height='auto'/>
+
+
           </div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/pJ52MzGns3Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
       </nav>
       <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
         <div className="rounded-md shadow">
-          <a href="https://www.bikeparks.co.nz/" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+          <a href="https://www.bikeparks.co.nz/" target="_blank" alt='Woodhill Bike Park Link' className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
                 Woodhill Bike Park
             <br></br>
           </a>
@@ -22,18 +27,8 @@ function West () {
 
       <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
         <div className="rounded-md shadow">
-          <a href="gallery" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
-                Gallery
-          </a>
-        </div>
-        <div className="mt-3 sm:mt-0 sm:ml-3">
-          <a href="parks" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-green-900 bg-green-300 hover:bg-green-200 md:py-4 md:text-lg md:px-10">
-                Parks
-          </a>
-        </div>
-        <div className="mt-3 sm:mt-0 sm:ml-3">
-          <a href="clubs" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-200 md:py-4 md:text-lg md:px-10">
-                Clubs
+          <a href="/" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-800 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
+                Home
           </a>
         </div>
       </div>
